5641bca0f0f5acbaffe4b42fbdda5866e7040743
stillouyng <sapranovmihail1@icloud.com>
2025-07-13 04:33:11 +0500

    Initialize core (only dependencies for now)

---
+++ src/core/__init__.py
@@ -0,0 +1,10 @@
+from .dependencies import (
+    ComplaintRepositoryDep, ComplaintServiceDep,
+    ApiIPClientDep, ApiLayerClientDep
+)
+
+
+__all__ = [
+    "ComplaintRepositoryDep", "ComplaintServiceDep",
+    "ApiLayerClientDep", "ApiIPClientDep",
+]
